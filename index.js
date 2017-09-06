3b0d315151460ea26010a58a893bb8b665460975
Preston Hunt <ph0859241@otc.edu>
2017-09-06 13:12:44 -0500

    now with RGB

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const chalk = require('chalk');
 
  
-console.log(chalk.blue('Happy birthday to you ') + chalk.green('Happy birthday to you ') + chalk.magenta('Happy birthday to Adam Goose ') + chalk.red('Happy birthday to you '));
+console.log(chalk.blue('Happy birthday to you ') + chalk.green('Happy birthday to you ') + chalk.magenta('Happy birthday to Adam Goose ') + chalk.rgb(255, 136, 0)('Happy birthday to you '));
+
+
