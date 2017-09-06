571e40e86f47031e7d52f8387a79bbcb579f6377
Preston Hunt <ph0859241@otc.edu>
2017-09-06 12:58:19 -0500

    Happy birthday

---
+++ index.js
@@ -0,0 +1,4 @@
+const chalk = require('chalk');
+
+ 
+console.log(chalk.blue('Happy birthday to you ') + chalk.green('Happy birthday to you ') + chalk.magenta('Happy birthday to Adam Goose ') + chalk.red('Happy birthday to you '));
